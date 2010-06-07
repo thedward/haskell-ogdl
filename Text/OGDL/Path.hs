8cd688488439e6e0fdaa556d60b380c16448ed73
thedward <thedward@barsoom.net>
2010-06-07 00:55:52 -0500

    first awful stab at compilepath
    Ignore-this: 28c4dbaf152343755eade14e960c04be
    
    darcs-hash:20100607055552-c2ee8-eefb8747c73c1edd2ac78874ce5a8393355c396f.gz

--- Text/OGDL/Path.hs
+++ Text/OGDL/Path.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE FlexibleInstances,MultiParamTypeClasses,TypeSynonymInstances #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {- reference: http://ogdl.sourceforge.net/spec/ogdl-path.htm -}
 
 module Text.OGDL.Path where
 
 import Text.OGDL.Combinators
 import Text.OGDL.Parsec (parseOGDL)
+import Text.Parsec
+import Data.List.Split
 
-
+-- This is really awful
+compilePath = concatMap children . concatMap  (either (const []) return . parseOGDL "") . splitOn "." 
 
